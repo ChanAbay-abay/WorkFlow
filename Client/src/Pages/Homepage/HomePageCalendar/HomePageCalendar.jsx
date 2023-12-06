9a0a6b68c3dcd7ea095b3d68a960614df3fe2421
chanchan <111845681+ChanAbay-abay@users.noreply.github.com>
2023-12-07 04:26:06 +0800

    Temporary Calendar Added

--- Client/src/Pages/Homepage/HomePageCalendar/HomePageCalendar.jsx
+++ Client/src/Pages/Homepage/HomePageCalendar/HomePageCalendar.jsx
@@ -1,14 +1,28 @@
 import React from "react";
+import Calendar from "react-calendar";
+import "./HomePageCalendar.css";
+import { useState } from "react";
+// import "react-calendar/dist/Calendar.css";
 import "./HomePageCalendar.css";
 
 function HomePageCalendar() {
+  const [date, setDate] = useState(new Date());
+
+  const onChange = () => {
+    setDate(date);
+  };
+
   return (
     <div className="hp-calendar-container">
-      <h1 className="hp-calendar-title">HomePageCalendar</h1>
+      <h1 className="hp-calendar-title">Your Calendar</h1>
+      <Calendar onChange={onChange} value={date}></Calendar>
+      {/* {console.log(date)} */}
+      {/* {date.toString} */}
     </div>
   );
 }
 
 export default HomePageCalendar;
 
-// https://www.youtube.com/watch?v=WutUO81wE90
+// https://www.youtube.com/watch?v=eM8n55lad1c&t=28s
+// https://www.npmjs.com/package/react-calendar?activeTab=readme
