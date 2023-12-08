aa43fcbc1e174c54fc96a6f9b8cf9d1ff71550f0
chanchan <111845681+ChanAbay-abay@users.noreply.github.com>
2023-12-08 16:40:32 +0800

    Creating Personal Task Page
    Since the last took too long. ://

--- Client/src/Pages/Personal/Personal.jsx
+++ Client/src/Pages/Personal/Personal.jsx
@@ -6,26 +6,17 @@ import NavBar from "../../Components/NavBar/NavBar";
 function Personal() {
   const username = "username";
   return (
-    // <div className="ptl-container">
-    //   <div className="ptl-nav">
-    //     <NavBar></NavBar>
-    //   </div>
-    //   <div className="ptl-content">
-    //     <div className="ptl-top">
-    //       <h1>top</h1>
-    //     </div>
-    //     <div className="ptl-bottom">
-    //       <h1>bottom</h1>
-    //     </div>
-    //   </div>
-    // </div>
-
     <div className="ptl-container">
       <div className="ptl-nav">
         <NavBar></NavBar>
       </div>
       <div className="ptl-content">
-        <h1>test</h1>
+        <div className="ptl-content-top">
+          <h1>top</h1>
+        </div>
+        <div className="ptl-content-bottom">
+          <h1>bottom</h1>
+        </div>
       </div>
     </div>
   );
