4eadea2759260efd8367c0af54a414bca8e1d823
chanchan <111845681+ChanAbay-abay@users.noreply.github.com>
2023-12-19 15:41:41 +0800

    Restarting Back End
    Restarting Backend from Zero. Thank you useless groupmates.

--- Client/src/api/endpoint.js
+++
@@ -1,8 +0,0 @@
-const Endpoints = {
-    CREATE_TASKS: 'task/createTask',
-    TASK_LISTS: 'task/listTask',
-    DONE_TASK: 'task/DoneTask',
-    UPDATE_TASK: 'task/updateTask'
-}
-
-export default Endpoints
