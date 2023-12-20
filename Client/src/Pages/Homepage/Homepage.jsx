e1aa9fc4d643cda4798766cbc61c0aee055cf4f6
chanchan <111845681+ChanAbay-abay@users.noreply.github.com>
2023-12-21 02:30:53 +0800

    Homepage Progression
    Homepage now displays username when logged in.

--- Client/src/Pages/Homepage/Homepage.jsx
+++ Client/src/Pages/Homepage/Homepage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import userDefault from "../../Assets/ICONS/userDefault.jpg";
 import "./Homepage.css";
 import JoinOrCreateTeam from "../../Components/JoinOrCreateTeam/JoinCreateTeam";
@@ -7,9 +7,37 @@ import TaskReport from "../../Components/TaskReport/TaskReport";
 import HomePageCalendar from "../../Components/HomePageCalendar/HomePageCalendar";
 import NavBar from "../../Components/NavBar/NavBar";
 import { useState } from "react";
+import { jwtDecode } from "jwt-decode";
 
 function Homepage() {
   const username = "Panelists";
+  const [userData, setUserData] = useState(null);
+
+  useEffect(() => {
+    const caughtToken = localStorage.getItem("token");
+    const token = jwtDecode(caughtToken);
+
+    console.log(caughtToken, token);
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `http://localhost:3000/api/users/${token.user.id}`
+        );
+
+        if (response.ok) {
+          const userData = await response.json();
+          setUserData(userData);
+          console.log(userData);
+        } else {
+          console.error("Error fetching user data");
+        }
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <div className="homepage-container">
@@ -20,7 +48,8 @@ function Homepage() {
         <div className="homepage-top">
           <div className="homepage-greet">
             <img src={userDefault} alt="" className="homepage-greet-avatar" />
-            <h1>Hello {username},</h1>
+            {/* <h1>Hello {username},</h1> */}
+            <h1>Hello {userData && userData.userName},</h1>
           </div>
           <div className="homepage-join-create">
             <JoinOrCreateTeam />
